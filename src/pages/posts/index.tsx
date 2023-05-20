6fe25f1db73b97b8cccfa2ed080a910bd5865731
Thiagosdep <thiago_sousap@hotmail.com>
2023-05-20 16:00:02 -0300

    feat: add SSG in Posts component

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,34 @@
+import { GetStaticProps } from 'next';
+
+interface IPost {
+  id: string;
+  title: string;
+}
+
+interface IPostsProps {
+  posts: IPost[];
+}
+
+export default function Posts({ posts }: IPostsProps) {
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<IPostsProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts: IPost[] = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
