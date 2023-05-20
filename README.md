602cdbdd773f3479fd9a348182d946b7f807bcb5
Thiagosdep <thiago_sousap@hotmail.com>
2023-05-20 15:40:13 -0300

    feat: add SSR in home page

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fake Api (json-server)
+
+- to run => npx json-server api.json -p 3333 -w
+- to run with delay => npx json-server api.json -p 3333 -w -d 2000
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
-import styles from '../styles/home.module.scss';
+import { GetServerSideProps } from 'next';
 
-export default function Home() {
+interface IPost {
+  id: string;
+  title: string;
+}
+
+interface IHomeProps {
+  posts: IPost[];
+}
+
+export default function Home({ posts }: IHomeProps) {
   return (
-    <h1>
-      Olá <span>Dev!</span>
-    </h1>
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<IHomeProps> = async () => {
+  const response = await fetch('http://localhost:3333/posts');
+  const posts: IPost[] = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
