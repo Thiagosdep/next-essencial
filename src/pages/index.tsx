41b298f75583b6e01fa7130fed9cb41d1021f4de
Thiagosdep <thiago_sousap@hotmail.com>
2023-05-20 13:49:29 -0300

    build: add scss and _document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Olá Dev!</h1>
-    </div>
+    <h1 className={styles.title}>
+      Olá <span>Dev!</span>
+    </h1>
   );
 }
